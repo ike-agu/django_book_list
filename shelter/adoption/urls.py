f14da7851ab80158e4fae6fbc9dd14ccff334531
Ikenna Agulobi <ikennaww@yahoo.fr>
2022-04-28 07:33:59 +0100

    created view for books and urls

--- shelter/adoption/urls.py
+++ shelter/adoption/urls.py
@@ -4,8 +4,9 @@ from . import views
 urlpatterns = [
     # we give the name arguments to path so we can use this urls in other  parts our the app .
     # we have to create a list of url paths
-    # the path need 2 mandatory parameters ie routes and HTTP object s
-    path("", views.home, name="adoption-home"),
-    path("authors/", views.authors, name="adoption-authors"),
+    # the path need  mandatory parameters ie routes and HTTP object s
+    path('', views.home, name='adoption-home'),
+    path('authors/', views.authors, name='adoption-authors'),
+    path('books/', views.show, name='adoption-show'),
+    path('books/<int:id>/', views.show, name='adoption-show')
     ]
- 
--- shelter/adoption/views.py
+++ shelter/adoption/views.py
@@ -1,3 +1,7 @@
+# we can use django.http or django.shortcuts to import HttpResponse
+# - django shortscuts is a collection of helper function that are generally used in view function/classes. there are many shortcuts available in module django shortcuts
+# => from django.http import HttpResponse
+
 from django.shortcuts import HttpResponse
 
 # Create your views here.
@@ -8,9 +12,18 @@ books = [
     {'id': 3, 'title': 'The No. 1 Ladies\' Detective Agency', 'author': 'Alexander McCall Smith'}
 ]
 
-
+# anytime you create methods in view, you need  to go create the url in urls.py
 def home(request):
-    return HttpResponse("<h1>List of book to view </h1>")
+    return HttpResponse("<h1>List of book to view </h1>"
+    f"<h3>The books in our list are {len(books)} in total </h3>"
+    )
+
+def show(request, id):
+    book = list(filter(lambda book:book['id'] == id, books))
+    return HttpResponse(
+        f"<h3>This page is for book number with id {id}</h3>"
+        f"<p>The book  name is {book[0]['title']}.</p>"
+    )
 
 def authors(request):
     return HttpResponse(f"<h1>List of Book authors</h1>")
