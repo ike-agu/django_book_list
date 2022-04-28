739bb295e4d19c6d336b95e46ac1ae94e7cff64c
Ikenna Agulobi <ikennaww@yahoo.fr>
2022-04-28 07:55:13 +0100

    created tempaltes folder and a base template file

--- shelter/adoption/urls.py
+++ shelter/adoption/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # we give the name arguments to path so we can use this urls in other  parts our the app .
+    # we give the name arguments to paths so we can use this urls in other  parts our the app .
     # we have to create a list of url paths
     # the path need  mandatory parameters ie routes and HTTP object s
     path('', views.home, name='adoption-home'),
     path('authors/', views.authors, name='adoption-authors'),
     path('books/', views.show, name='adoption-show'),
+    # in the path below we use a path converter provided by django to  to access single book by id in the browser like so http://127.0.0.1:8000/adoption/books/1/
     path('books/<int:id>/', views.show, name='adoption-show')
-    ]
+]
